d75c972920df67016b09a265cde8bb5c35c65f2f
kikass13 <nickfiege999@gmail.com>
2022-03-03 11:43:01 +0100

    expanded source location example

--- sourcelocation/main.cpp
+++ sourcelocation/main.cpp
@@ -1,39 +1,48 @@
 #include <iostream>
-#include <source_location>
+#include <experimental/source_location>
 
 using source_l = std::experimental::source_location;
 
-template <typename... Ts>
+template <int I, typename... Ts>
 struct debug
 {
     debug(Ts&&... ts, 
         const source_l& loc = source_l::current())
     {
-        std::cout << loc.function_name() << " line " << loc.line() << ": ";
+        std::cout << I << loc.function_name() << " line " << loc.line() << ": ";
         ((std::cout << std::forward<Ts>(ts) << " "), ...);
         std::cout << std::endl;
     }
 };
-
 template <typename... Ts>
-debug(Ts&&...args) -> debug<Ts...>;
+struct debugA : debug<1, Ts...>{
+    using debug<1, Ts...>::debug;
+};
+template <typename... Ts>
+debugA(Ts&&...args) -> debugA<Ts...>;
+template <typename... Ts>
+struct debugB : debug<2, Ts...>{
+    using debug<2, Ts...>::debug;
+};
+template <typename... Ts>
+debugB(Ts&&...args) -> debugB<Ts...>;
 
 void test()
 {
-    debug();
-    debug(9);
+    debugA();
+    debugA(9);
 }
 
 void foo(const source_l& loc = source_l::current())
 {
-    debug();
-    debug<int>(666, loc);
+    debugA();
+    debugA<int>(666, loc);
 }
 
 int main()
 {
-    debug(5, 'A', 3.14f, "foo");
+    debugB(5, 'A', 3.14f, "foo");
     test();
     foo();
-    debug("bar", 123, 2.72);
+    debugB("bar", 123, 2.72);
 }
