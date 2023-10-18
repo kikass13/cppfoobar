8c98a3b310d4d6b3b2ecdcfa409835a882c9ca55
kikass13 <nickfiege999@gmail.com>
2023-10-18 19:23:19 +0200

    some cleanup and more examples

--- dbscan_clustering/run.sh
+++ dbscan_clustering/run.sh
@@ -2,9 +2,11 @@
 
 # ./build/dbscan.bin sample2d.csv 0.2 10 > output.csv
 
-./build/dbscan.bin sample3d.csv 1 10 > output.csv && python3 plot.py
+# ./build/dbscan.bin sample3d.csv 1 10 > output.csv && python3 plot.py
 
-# ./build/dbscan.bin 3d_pointcloud_kitti.csv 1 10 > output.csv && python3 plot.py
+# ./build/dbscan.bin depth_image_cloud.csv 1 10 > output.csv && python3 plot.py
+./build/dbscan.bin kitti_cloud.csv 1 10 > output.csv && python3 plot.py
+
 ### needs ~ the same time as the python wrapper of skicit.learn dbscan
 
 
